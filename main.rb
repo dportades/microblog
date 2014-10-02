5c36be25492e2902b6f215a9ec7cacc39e992e1b
Anh La Russo <Aphuong85@gmail.com>
2014-10-02 15:37:33 -0400

    added sign in form

--- main.rb
+++ main.rb
@@ -10,6 +10,32 @@ enable :sessions
 use Rack::Flash, :sweep => true
 set :sessions => true
 
+def set_current_user
+  if session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
+end
+
 get '/home' do
   erb :home
+end
+
+get '/' do
+  erb :sign_in
+end
+
+post '/in' do
+  email = params[:user][:email]
+  password = params[:user][:password]
+
+  @user = User.find_by(email: email)
+
+  if @user.password == password
+    flash[:notice] = 'You’ve been signed in successfully.'
+    session[:user_id] = @user.id
+    redirect '/home'
+  else
+    flash[:alert] = "There was a problem with the log in."
+    redirect '/error'
+  end
 end
